f9e20e95163b8222e09b30edea71bf6b5a3d9e77
Andrew Almeida <100097231+andrewalmeidaa@users.noreply.github.com>
2023-04-22 14:49:57 -0300

    add content

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function openNav(){
+    document.getElementById('myNav').style.height = '100%'
+    // pra alterar se vai sair dos lados ou de cima e so mudar height para width
+}
+
+function closeNav(){
+    document.getElementById('myNav').style.height = '0'
+}
+
+function optionSelected(){
+    closeNav()
+}
